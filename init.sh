e3a5eec18af5dfa27acb13314fe3c39b871f69ee
izverg23 <izverg_@bk.ru>
2016-02-23 14:33:21 +0300

    first

---
+++ init.sh
@@ -0,0 +1,4 @@
+sudo ?ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
