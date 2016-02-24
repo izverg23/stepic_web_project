e61c3d645659e32af4bba1230c3566f2f7437138
izverg23 <izverg_@bk.ru>
2016-02-24 16:14:47 +0300

    works!!!

--- hello.py
+++ hello.py
@@ -1,22 +1,22 @@
-from urlparse import parse_qs
+from cgi import parse_qs                                                        
 
-def application(environ, start_response):                                       
+def application(environ, start_response):
 
 	queryString = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
 	body = ''
 	print(queryString)
 	for key, value in queryString.items() :
-		body += key + ' = '
 		for element in value :
-			body += element + '\n'
-	
-	print(body)
-	status = '200 OK'                                                           
-	
-	response_headers = [                                                        
-		('Content-Type', 'text/plain'),                                         
-	]                                                                           
+			body += key + '='                                       
+			body += element + '\r\n'
 
+	print(body)                                                             
+		
+	status = '200 OK'
+
+	response_headers = [
+		('Content-Type', 'text/plain'),
+	]
 	start_response(status, response_headers)                                    
 
 	return [body]
