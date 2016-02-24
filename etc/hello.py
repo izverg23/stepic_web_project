8c63a7e0f4c95372abf8939fee5e0d27b6a9c3f6
izverg23 <izverg_@bk.ru>
2016-02-24 09:54:26 +0300

    setting for d

--- etc/hello.py
+++ etc/hello.py
@@ -3,7 +3,7 @@ CONFIG = {
     'working_dir': '/home/box/web',
     'python': '/usr/bin/python',
     'args': (
-        '--bind=127.0.0.1:8080',
+        '--bind=0.0.0.0:8080',
         '--workers=16',
         '--timeout=60',
 		'--log-level=debug',
--- hello.py
+++ hello.py
@@ -2,13 +2,13 @@ def hello(environ, start_response) :
 	status = '200 OK'
 	headers = [ ('Content-Type', 'text/plan') ]
 	
-	queryString = environ.QUERY_STRING
-	queryList = queryString.split('&')
-	body = ''
-	i = 0
-	for element in queryList :
-		body += element[i] + '<br>'
-		i += 1
+	#queryString = environ.QUERY_STRING
+	#queryList = queryString.split('&')
+	body = 'Hello World'
+	#i = 0
+	#for element in queryList :
+	#	body += element[i] + '<br>'
+	#	i += 1
 
 	print body
 	start_response(status, headers)
