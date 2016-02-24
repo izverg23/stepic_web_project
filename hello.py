7bf5f8f57f99314bfa7eaf25decd05723b8b29de
izverg23 <izverg_@bk.ru>
2016-02-24 10:25:58 +0300

    new app

--- hello.py
+++ hello.py
@@ -1,15 +1,45 @@
-def hello(environ, start_response) :
-	status = '200 OK'
-	headers = [ ('Content-Type', 'text/plan') ]
-	
-	#queryString = environ.QUERY_STRING
-	#queryList = queryString.split('&')
-	body = 'Hello World'
-	#i = 0
-	#for element in queryList :
-	#	body += element[i] + '<br>'
-	#	i += 1
+from __future__ import unicode_literals
+import multiprocessing
+import gunicorn.app.base
+from gunicorn.six import iteritems
 
-	print body
-	start_response(status, headers)
-	return body
+def number_of_workers():
+    return (multiprocessing.cpu_count() * 2) + 1
+
+
+def handler_app(environ, start_response):
+    response_body = 'Works'
+    status = '200 OK'
+
+    response_headers = [
+        ('Content-Type', 'text/plain'),
+    ]
+
+    start_response(status, response_headers)
+
+    return [response_body]
+
+
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
+
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super(StandaloneApplication, self).__init__()
+
+    def load_config(self):
+        config = dict([(key, value) for key, value in iteritems(self.options)
+                       if key in self.cfg.settings and value is not None])
+        for key, value in iteritems(config):
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+
+if __name__ == '__main__':
+    options = {
+        'bind': '%s:%s' % ('127.0.0.1', '8080'),
+        'workers': number_of_workers(),
+    }
+    StandaloneApplication(handler_app, options).run()
