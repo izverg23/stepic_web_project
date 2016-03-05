8d2b30f0b9f9f363c35b4243dc0dd4f2e9830d9d
izverg23 <izverg_@bk.ru>
2016-03-06 00:23:02 +0300

    blin 3

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('qa.views',
-	#url(r'^', 'test', name='index'),
+	url(r'^$', 'test', name='index'),
 	url(r'^login/$', 'test', name='login'),
 	url(r'^signup/$', 'test', name='signup'),
 	url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),
-	url(r'^ask/$', include("ask.urls")),
+	url(r'^ask/', 'test', name='ask'),
 	url(r'^popular/$', 'test', name='popular'),	
 	url(r'^new/$', 'test', name='new'),	
 )
