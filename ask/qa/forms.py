b9b03a1b50213a494a409c64e37510a75ab27997
izverg23 <izverg_@bk.ru>
2016-03-13 04:41:01 +0300

    form_v0.1

---
+++ ask/qa/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from qa.models import Question, Answer
+
+class AskForm(forms.Form) :
+	title = forms.CharField(max_length=255)
+	text = forms.CharField(widget=forms.Textarea)
+	
+	def clean_title(self) :
+		title = self.cleaned_data['title']
+		return title
+
+	def clean_text(self) :
+		text = self.cleaned_data['text']
+		return text
+		
+	def save(self) :
+		quest = Question(title=self.title, text=self.text)
+		quest.save()
+		return quest
+	
+class AnswerForm(forms.Form) :
+	text = forms.CharField(widget=foms.Textarea)
+	question = forms.CharField(widget=forms.HiddenInput)
+	
+	def clean_text(self) :
+		text = self.cleaned_data['text']
+		return text
+		
+	def clean_question(self) :
+		try :
+			quest_id = int(self.cleaned_data['question'])
+		except ValueError :
+			raise forms.ValidationError('fail input')
+		return quest_id
+	
+	def save(self) :
+		answer = Answer(text=self.text, question=self.question)
+		answer.save()
+		return answer
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
 from qa.models import Question, Answer
+from qa.forms import AskForm, AnswerForm
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
 def index(request) :
-	print("HELLO_!!!!!!!!!")
 	pageLimit = 10
 	#Entry.objects.order_by(Coalesce('summary', 'headline').desc()) #asc()
 	qwests = Question.objects.all().order_by('-id')
@@ -66,9 +66,32 @@ def question(request, quest_id) :
 	answers = Answer.objects.all().filter(question = quest)
 	
 	title = 'qwest ' + quest_id
+	form = AnswerForm(question=quest_id)
 	
 	return render(request, 'question.html', {
 		'title' : title,
 		'question' : quest,
 		'list' : answers,
+		'form' : form,
 	})
+
+def ask(request) :
+	if request.method == "POST" :
+		form = AskForm(request.POST)
+		if form.is_valid():
+			quest = form.save()
+			url = quest.get_url()
+			return HttpResponseRedirect(url)
+	else :
+		form = AskForm()
+	return render(request, 'sdk_sdd.html', {
+		'form' : form
+	})
+	
+def answer(request) :
+	if request.method == "POST" :
+		form = AnswerForm(request.POST)
+		if form.is_valid():
+			answer = form.save()
+			url = '/question/' + form.question
+			return HttpResponseRedirect(url)
